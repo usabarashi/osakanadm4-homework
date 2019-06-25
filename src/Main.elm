bc7a21b475c7e4143c8bce734b7901cbe4de8159
gen <usabarashi@mac.com>
2019-06-25 22:53:13 +0900

    SPAの一歩手前の処まで整理した
    Reference難しいから普通のListにした

--- src/Main.elm
+++ src/Main.elm
@@ -6,9 +6,12 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Page.Photo
-import Query.PhotoQuery as PhotoQuery
-import Reference exposing (Reference)
-import Reference.List
+import Query.PhotoQuery
+import Types.Css
+
+
+
+-- Main
 
 
 main =
@@ -20,74 +23,52 @@ main =
         }
 
 
-init : () -> ( Model, Cmd Msg )
+init : () -> ( Page, Cmd Msg )
 init () =
-    update (MapToPhotoQuery PhotoQuery.GetRequest) create
-
-
-
--- Model
-
-
-type alias Model =
-    { photos : List Photo }
-
-
-initModel : Model
-initModel =
-    { photos = [] }
-
-
-create : Model
-create =
-    initModel
+    let
+        ( initPageModel, initPageCmd ) =
+            Page.Photo.init
+    in
+    ( PhotoPage initPageModel
+    , Cmd.map PhotoPageMsg initPageCmd
+    )
 
 
 
 -- Update
 
 
+type Page
+    = PhotoPage Page.Photo.Model
+
+
 type Msg
-    = -- View
-      MapToPhotoPage (Reference Photo (List Photo)) Photo.Msg
-      -- Query
-    | MapToPhotoQuery PhotoQuery.Msg
+    = -- Page case
+      PhotoPageMsg Page.Photo.Msg
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
+update : Msg -> Page -> ( Page, Cmd Msg )
+update msg page =
     case msg of
-        -- View
-        MapToPhotoPage refPhoto photoMsg ->
-            let
-                ( resultPhoto, resultCmd ) =
-                    Photo.update photoMsg (Reference.this refPhoto)
-
-                modifyPhotos : Photo -> Reference Photo (List Photo) -> List Photo
-                modifyPhotos modifiedEntity refEntity =
-                    Reference.root <| Reference.modify (\discard -> modifiedEntity) refEntity
-
-                resultPhotos : List Photo
-                resultPhotos =
-                    modifyPhotos resultPhoto refPhoto
-            in
-            ( { model | photos = resultPhotos }, Cmd.map (MapToPhotoPage refPhoto) resultCmd )
-
-        -- Query
-        MapToPhotoQuery queryMsg ->
-            let
-                ( gotPhotos, resultCmd ) =
-                    PhotoQuery.update queryMsg
-            in
-            ( { model | photos = gotPhotos }, Cmd.map MapToPhotoQuery resultCmd )
+        -- Page case
+        PhotoPageMsg pageMsg ->
+            case page of
+                PhotoPage pageModel ->
+                    let
+                        ( modifiedPageModel, pageCmd ) =
+                            Page.Photo.update pageMsg pageModel
+                    in
+                    ( PhotoPage modifiedPageModel
+                    , Cmd.map PhotoPageMsg pageCmd
+                    )
 
 
 
 -- Subscriptions
 
 
-subscriptions : Model -> Sub Msg
-subscriptions model =
+subscriptions : Page -> Sub Msg
+subscriptions page =
     Sub.none
 
 
@@ -95,53 +76,11 @@ subscriptions model =
 -- View
 
 
-view : Model -> Html Msg
-view model =
-    let
-        headerAttribute : List (Attribute msg)
-        headerAttribute =
-            [ style "position" "fixed"
-            , style "top" "0"
-            , style "left" "0"
-            , style "width" "100%"
-            , style "height" "40px"
-            , style "borderBottom" "1px solid silver"
-            , style "boxSizing" "border-box"
-            , style "display" "flex"
-            , style "alignItems" "center"
-            , style "backgroundColor" "white"
-            ]
-
-        titleAttribute : List (Attribute msg)
-        titleAttribute =
-            [ style "fontSize" "large"
-            , style "fontStyle" "italic"
-            , style "margin" "0"
-            ]
-
-        photoViews : List Photo -> Html Msg
-        photoViews photos =
-            let
-                mappedPhotoViews : Html Msg
-                mappedPhotoViews =
-                    Reference.top photos
-                        |> Reference.List.unwrap mappedPhotoView
-                        |> div photosAttribute
-
-                photosAttribute : List (Attribute msg)
-                photosAttribute =
-                    [ style "width" "500px"
-                    , style "margin" "65px auto"
-                    ]
-
-                mappedPhotoView : Reference Photo (List Photo) -> Html Msg
-                mappedPhotoView refPhoto =
-                    Page.Photo.view (Reference.this refPhoto) Photo.CountUpFavorite
-                        |> Html.map (MapToPhotoPage refPhoto)
-            in
-            mappedPhotoViews
-    in
+view : Page -> Html Msg
+view page =
     div []
-        [ header headerAttribute [ h1 titleAttribute [ text "Osakanagram" ] ]
-        , photoViews model.photos
+        [ header Types.Css.headerAttribute [ h1 Types.Css.titleAttribute [ text "Osakanagram" ] ]
+        , case page of
+            PhotoPage pageModel ->
+                Html.map PhotoPageMsg <| Page.Photo.view pageModel
         ]
--- src/page/Photo.elm
+++ src/page/Photo.elm
@@ -1,27 +1,118 @@
-module Page.Photo exposing (view)
+module Page.Photo exposing (Model, Msg(..), init, update, view)
 
 import Adapter.OsakanadmApi
 import Domain.Photo as Photo exposing (Photo)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Http
+import Query.PhotoQuery
+import Task
 
 
-view : Photo -> msg -> Html msg
-view photo msg =
-    div [ style "border" "1px solid silver", style "margin" "25px 0" ]
-        [ div [] [ img [ style "width" "100%", src <| Adapter.OsakanadmApi.url ++ photo.image ] [] ]
-        , div [ style "padding" "0 5px" ] [ text photo.title ]
-        , div [ style "padding" "0 5px" ]
-            [ button
-                [ onClick msg
+
+-- Model
+
+
+type alias Model =
+    { photos : List Photo }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { photos = [] }
+    , Task.attempt GetPhotosResponse <| Query.PhotoQuery.getPhotos
+    )
+
+
+
+-- Update
+
+
+type Msg
+    = -- View case
+      CountUpFavorite Photo
+      -- Query case
+    | GetPhotosRequest
+    | GetPhotosResponse (Result Http.Error (List Photo))
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        -- View case
+        CountUpFavorite modifyPhoto ->
+            let
+                modifiedPhotos : List Photo
+                modifiedPhotos =
+                    List.map
+                        (\photo ->
+                            case photo.image == modifyPhoto.image of
+                                False ->
+                                    photo
+
+                                True ->
+                                    Photo.countUpFavorite modifyPhoto
+                        )
+                        model.photos
+            in
+            ( { model | photos = modifiedPhotos }, Cmd.none )
+
+        -- Query case
+        GetPhotosRequest ->
+            ( model, Task.attempt GetPhotosResponse Query.PhotoQuery.getPhotos )
+
+        GetPhotosResponse (Ok photos) ->
+            ( { model | photos = photos }, Cmd.none )
+
+        GetPhotosResponse (Err _) ->
+            ( model, Cmd.none )
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    let
+        photosAttribute : List (Attribute msg)
+        photosAttribute =
+            [ style "width" "500px"
+            , style "margin" "65px auto"
+            ]
+    in
+    div photosAttribute <| List.map (\photo -> photoView photo (CountUpFavorite photo)) model.photos
+
+
+photoView : Photo -> msg -> Html msg
+photoView photo msg =
+    let
+        favoriteImageSrc : Photo -> Attribute msg
+        favoriteImageSrc photo_ =
+            src <| Adapter.OsakanadmApi.url ++ photo_.image
+
+        favoriteButton : Photo -> msg -> Html msg
+        favoriteButton photo_ msg_ =
+            button
+                [ onClick msg_
                 , style "cursor" "pointer"
                 , style "border" "0"
                 , style "fontSize" "large"
                 , style "backgroundColor" "transparent"
-                , style "color" photo.favorite.color
+                , style "color" photo_.favorite.color
                 ]
-                [ text photo.favorite.icon ]
-            , span [] [ text "いいね！", text <| String.fromInt photo.favorite.count, text "件" ]
+                [ text photo_.favorite.icon ]
+
+        favoriteCount : Photo -> Html msg
+        favoriteCount photo_ =
+            text <| String.fromInt photo_.favorite.count
+    in
+    div [ style "border" "1px solid silver", style "margin" "25px 0" ]
+        [ div [] [ img [ style "width" "100%", favoriteImageSrc photo ] [] ]
+        , div [ style "padding" "0 5px" ] [ text photo.title ]
+        , div [ style "padding" "0 5px" ]
+            [ favoriteButton photo msg
+            , span [] [ text "いいね！", favoriteCount photo, text "件" ]
             ]
         ]
